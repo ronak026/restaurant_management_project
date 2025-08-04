30949ff4bf990c66eb4d5dc3661e2ff980c38da2
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-04 15:45:42 +0530

    19859

--- account/models.py
+++ account/models.py
@@ -6,4 +6,4 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.user.username
+        return self.user.get_username
