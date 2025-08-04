6681679a7075edfbdad14fe768dd791eda470a98
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-04 19:16:08 +0530

    19859

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # this is user where i add onetoonefield relationship where one user one profile
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Links to built-in user 
     phone_number = models.CharField(max_length=10)
     
     def __str__(self):
