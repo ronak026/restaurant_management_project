47f9d3245097fd19c1dd81ada54a72f130c99db2
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-04 15:48:35 +0530

    19859

--- account/models.py
+++ account/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # this is user where i add onetoonefield relationship where one user one profile
+    phone_number = models.CharField(max_length=10) # in built in user model there has no phone number fiels so i add one
 
     def __str__(self):
-        return self.user.get_username
+        return self.user.get_username()
