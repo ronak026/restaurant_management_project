e796267472bd09de0d83b815cf1d6918dcee4f10
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 21:47:26 +0530

    19860

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    response = request.get('http://localhost:8000/menu/')
+    menu_items = response.json() if response.status_code == 200 else []
+
+    return render(request, 'home.html', {'menu_items':menu_items})
