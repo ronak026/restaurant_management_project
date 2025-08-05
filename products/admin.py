bc83fb4374db249d0f4c0ce81a270cbdcb350d5f
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 22:20:30 +0530

    19862

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import *
+from prosucts.models import *
 
 # Custom Admins
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
-    search_fields = ('item_name')
+    search_fields = ('item_name',)
 
 # Register your models here.
 admin.site.register(Item,ItemAdmin)
