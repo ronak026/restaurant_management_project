ae4fe6a80ad8c871c603d50b283f808cd210d909
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 20:31:15 +0530

    19862

--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    extra = 1 # how many blank order items to show on the order page
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','status','total_amount','created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer__username')
+    inlines = [OrderItemInline]
+
 admin.site.register(Order)
+admin.site.register(OrderAdmin)
