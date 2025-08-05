c7309604b3188364c4068e463de0146a069b7421
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 14:20:04 +0530

    19863

--- products/views.py
+++ products/views.py
@@ -24,3 +24,5 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class 
