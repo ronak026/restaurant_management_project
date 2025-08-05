1e1b182dbee030a56b8a97e101cde45881565180
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 20:15:09 +0530

    19861

--- products/models.py
+++ products/models.py
@@ -8,3 +8,11 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
