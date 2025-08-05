9d5dfb70d1f4cac317fb5901c07a241a060b026c
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 20:55:47 +0530

    19862

--- products/admin.py
+++ products/admin.py
@@ -5,8 +5,9 @@ from .models import *
 # Custom Admins
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
-
+    search_field = ('name')
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    
 admin.site.register(Item,ItemAdmin)
-# admin.site.register(Menu)
