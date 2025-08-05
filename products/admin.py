1d21c4d4d6f5d8675381e89d58ed035764bbf706
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 20:59:13 +0530

    19862

--- products/admin.py
+++ products/admin.py
@@ -8,6 +8,10 @@ class ItemAdmin(admin.ModelAdmin):
     search_field = ('name')
 
 # Register your models here.
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    list_display=('id','customer_name','status','total_amount','created_at')
+    search_field=('customer__username')
+    filter_horizontal=('items')
     
 admin.site.register(Item,ItemAdmin)
