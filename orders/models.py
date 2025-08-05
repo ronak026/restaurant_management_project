d9e35738d1470ecbb3439a0b88051baf8ea21082
Ronak Vekariya <ronakvekariya24@gmail.com>
2025-08-05 20:13:13 +0530

    19861

--- orders/models.py
+++ orders/models.py
@@ -2,18 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Order(models.model):
+
+
+class Order(models.Model):
     STATUS_CHOICES  = [
-        ('PENDING', 'panding'),
+        ('PENDING', 'pending'),
         ('PROCESSING', 'processing'),
         ('COMPLETED', 'completed'),
-        ('CANCELLED', 'cancelled'),
+        ('CANCELED', 'canceled'),
     ]
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    order_items = models.ManyToManyField(Menu, through='OrderItem')
-    total_amount = models.DecimalField(max_digits=10, decimal_palces=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
     created_at = models.DateTimeField(auto_now_add=True)
 
+class OrderItem(models.Model):
+    
     def __str__(self):
         return f"Order #{self.id} - {self.customer.username}"
